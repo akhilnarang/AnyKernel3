cbf76447a538aed857fac4fc8d05e0a7075b9674
Nathan Chancellor <natechancellor@gmail.com>
2018-01-02 18:38:48 -0700

    OP5: anykernel.sh: Add support for the OnePlus 5T
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,15 +4,15 @@
 ## AnyKernel setup
 # begin properties
 properties() {
-kernel.string=Flash Kernel for the OnePlus 5 by @nathanchance
+kernel.string=Flash Kernel for the OnePlus 5/T by @nathanchance
 do.devicecheck=1
 do.modules=1
 do.cleanup=1
 do.cleanuponabort=0
 device.name1=OnePlus5
-device.name2=oneplus5
+device.name2=OnePlus5T
 device.name3=cheeseburger
-device.name4=
+device.name4=dumpling
 device.name5=
 } # end properties
 
