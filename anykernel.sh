e4598cfc800e4d0abc1ece29320ce0808b682cb0
Akhil Narang <akhilnarang.1999@gmail.com>
2019-07-21 19:03:07 +0530

    anykernel.sh: rebrand to Illusion
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Derp Kernel by akhilnarang
+kernel.string=Illusion Kernel by akhilnarang
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
